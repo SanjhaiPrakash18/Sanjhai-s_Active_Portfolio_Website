04a954935c0d888c2e71629aca96fb49bc28bfeb
Sanjhai Prakash B <103559414+SanjhaiPrakash18@users.noreply.github.com>
2025-07-18 21:07:23 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'rethink': ['Rethink Sans', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
